76735604a3b0c1389bad2a89456bd65e1f7061a0
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-11-15 17:36:12 +0300

    finished 2nd test

--- test_homework.py
+++ test_homework.py
@@ -19,11 +19,11 @@ def test_rectangle():
     b = 20
 
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = a * 2 + b * 2
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a * b
     assert area == 200
 
 
